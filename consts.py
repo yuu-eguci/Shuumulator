752ac1d1d293f6ec7126dc371fe6bf45418a6c65
yuu-eguci <yuu.eguci@gmail.com>
2021-01-24 16:13:13 +0900

    ✨ Common modules

---
+++ consts.py
@@ -0,0 +1,46 @@
+"""
+Python やるときにいつもあって欲しい自分用モジュールです。
+"""
+
+# Built-in modules.
+import os
+import dotenv
+
+
+# .env をロードします。
+# 本スクリプトは .env がなくても動きます。(そのための raise_error_if_not_found です。)
+# NOTE: raise_error_if_not_found=False .env が見つからなくてもエラーを起こさない。
+dotenv.load_dotenv(dotenv.find_dotenv(raise_error_if_not_found=False))
+
+
+def get_env(keyname: str) -> str:
+    """環境変数を取得します。
+    GitHub Actions では環境変数が設定されていなくても yaml 内で空文字列が入ってしまう。空欄チェックも行います。
+
+    Arguments:
+        keyname {str} -- 環境変数名。
+
+    Raises:
+        KeyError: 環境変数が見つからない。
+
+    Returns:
+        str -- 環境変数の値。
+    """
+    _ = os.environ[keyname]
+    if not _:
+        raise KeyError(f'{keyname} is empty.')
+    return _
+
+
+MYSQL_HOST = get_env('MYSQL_HOST')
+MYSQL_USER = get_env('MYSQL_USER')
+MYSQL_PASSWORD = get_env('MYSQL_PASSWORD')
+MYSQL_DATABASE = get_env('MYSQL_DATABASE')
+SLACK_BOT_TOKEN = get_env('SLACK_BOT_TOKEN')
+SLACK_MESSAGE_CHANNEL = get_env('SLACK_MESSAGE_CHANNEL')
+
+if __name__ == '__main__':
+    print(repr(MYSQL_HOST))
+    print(repr(MYSQL_USER))
+    print(repr(MYSQL_PASSWORD))
+    print(repr(MYSQL_DATABASE))
---
+++ utils.py
@@ -0,0 +1,177 @@
+"""
+Python やるときにいつもあって欲しい自分用モジュールです。
+これは使いまわしたいのでリポジトリのビジネスロジック入れないでね。
+以下、できること。
+
+# Dependencies
+pipenv install python-dotenv mysql-connector-python
+
+# MySQL への接続。
+with utils.DbClient() as db_client:
+    records = db_client.sample_select()
+
+# logger の取得。
+logger = utils.get_my_logger(__name__)
+
+# Slack メッセージの送信。
+utils.send_slack_message(message)
+"""
+
+# Built-in modules.
+import logging
+
+# Third-party modules.
+import mysql.connector
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# User modules.
+import consts
+
+
+class DbClient:
+    """DB アクセスを行うクラスです。 with 構文で使用可能です。
+    with utils.DbClient() as db_client:
+        records = db_client.sample_select()
+    """
+
+    def __enter__(self):
+        mysql_connection_config = {
+            'host': consts.MYSQL_HOST,
+            'user': consts.MYSQL_USER,
+            'password': consts.MYSQL_PASSWORD,
+            'database': consts.MYSQL_DATABASE,
+        }
+        self.connection = mysql.connector.connect(**mysql_connection_config)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+    def sample_select(self) -> list:
+        """サンプルです。
+
+        Returns:
+            list: Select 結果のリスト。
+        """
+
+        select_sql = ' '.join([
+            'SELECT',
+                'id',  # noqa: E131
+            'FROM sampletable',
+            'WHERE',
+                'id = %s',
+            'ORDER BY id DESC',
+        ])
+        cursor = self.connection.cursor(dictionary=True)
+        cursor.execute(select_sql, (1,))
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    def sample_update(self):
+        """サンプルです。
+        """
+
+        update_sql = ' '.join([
+            'UPDATE sampletable',
+            'SET',
+                'foo = %s',  # noqa: E131
+            'WHERE id = %s',
+        ])
+        cursor = self.connection.cursor()
+        cursor.execute(update_sql, (1, 1))
+        cursor.close()
+        self.connection.commit()
+
+
+def get_placeholder(count: int) -> str:
+    """count ぶんのプレースホルダ文字列を作ります。
+    %s, %s, %s, %s, ...
+
+    Args:
+        count (int): 欲しい %s の数。
+
+    Returns:
+        str: %s, %s, %s, %s, ...
+    """
+
+    return ','.join(('%s' for i in range(count)))
+
+
+def get_my_logger(logger_name: str) -> logging.Logger:
+    """モジュール用のロガーを作成します。
+    logger = get_my_logger(__name__)
+
+    Args:
+        logger_name (str): getLogger にわたす名前。 __name__ を想定しています。
+
+    Returns:
+        logging.Logger: モジュール用のロガー。
+    """
+
+    """
+    メインの処理とは別に関係ない。
+
+    Returns:
+        Logger -- モジュール用のロガー。
+    """
+
+    # ルートロガーを作成します。ロガーはモジュールごとに分けるもの。
+    logger = logging.getLogger(logger_name)
+    # ルートロガーのログレベルは DEBUG。
+    logger.setLevel(logging.DEBUG)
+    # コンソールへ出力するハンドラを作成。
+    handler = logging.StreamHandler()
+    # ハンドラもログレベルを持ちます。
+    handler.setLevel(logging.DEBUG)
+    # ログフォーマットをハンドラに設定します。
+    formatter = logging.Formatter(
+        # NOTE: 改行は逆に見づらいので E501 を無視します。
+        '%(asctime)s - %(levelname)s - %(filename)s - %(name)s - %(funcName)s - %(message)s')  # noqa: E501
+    handler.setFormatter(formatter)
+    # ハンドラをロガーへセットします。
+    logger.addHandler(handler)
+    # 親ロガーへの(伝播をオフにします。
+    logger.propagate = False
+    return logger
+
+
+def send_slack_message(message: str) -> None:
+    """slack_sdk を用いたメッセージ送信を行います。
+    Document: https://github.com/slackapi/python-slack-sdk/blob/main/tutorial/01-creating-the-slack-app.md  # noqa: E501
+
+    Args:
+        message (str): 送信したいメッセージ。
+    """
+
+    slack_client = WebClient(token=consts.SLACK_BOT_TOKEN)
+
+    try:
+        # NOTE: unfurl_links は時折鬱陶しいと思っている「リンクの展開機能」です。不要です。 False.
+        response = slack_client.chat_postMessage(
+            channel=consts.SLACK_MESSAGE_CHANNEL,
+            text=message,
+            unfurl_links=False)
+        # NOTE: Slack api のドキュメントにあるコードですが、正常に終了しても false になる場合があるので注意。
+        #       リンクの含まれるメッセージを送信すると、返却値が勝手に変更されるため一致しません。
+        #       - リンクの前後に <, > がつく
+        #       - & -> &amp; エスケープが起こる
+        assert response['message']['text'] == message
+    except SlackApiError as e:
+        assert e.response['ok'] is False
+        # str like 'invalid_auth', 'channel_not_found'
+        assert e.response['error']
+        logger.error(f'Got an error: {e.response["error"]}')
+
+
+# utils モジュール用のロガーを作成します。
+logger = get_my_logger(__name__)
+
+if __name__ == '__main__':
+    logger.debug('でばーぐ')
+    logger.info('いんーふぉ')
+    logger.warning('うぉーにん')
+    logger.error('えろあ')
+    logger.fatal('ふぇーたる(critical と同じっぽい)')
+    logger.critical('くりてぃこぉ')
