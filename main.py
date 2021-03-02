62899fa97e73e649aa2e2ac1998b1a6522948404
yuu-eguci <yuu.eguci@gmail.com>
2021-03-02 20:40:52 +0900

    💡 コメントプログラムを追加

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ target_stocks = functions.get_target_stocks()
 logger.info(f'対象銘柄は {len(target_stocks)} 件です。')
 
 for stock in target_stocks:
+    # NOTE: stock は dict です。 { code, name }
 
     # スクレイピングで現在の価格を取得します。
     # NOTE: テスト中につき実際の値は取得せずテスト値で処理を進めています。
@@ -42,13 +43,21 @@ for stock in target_stocks:
     current_stock_price = decimal.Decimal('1377.0')
 
     # stock_log 保存。
+    # NOTE: これが必要なのかは微妙ですね。せっかく取得した情報がもったいないと思い、記録しています。
     with utils.DbClient() as db_client:
         tradings = db_client.create_stock_log(
             stock['id'],
             current_stock_price
         )
 
-    # 売る、買う、何もしない、を選択します。
+    # この stock の最新 trading 情報を取得します。
+
+    # 売却済みであれば、有無を言わさず買います。
+    # NOTE: sold_at があるということが売却済みの証です。
+    # NOTE: 買うということは trading に一件追加するということです。
+
+    # この stock の手持ちがある場合は、売るかどうかの判断に進みます。
+    # 売るのは、利確ラインを超えているとき、あるいは損切ラインを下回っているときです。
 
     # スクレイピング先に負荷をかけることを避けるため、待機します。
     time.sleep(5)
